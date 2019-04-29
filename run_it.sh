c513ff63bdab4a81cb62fa808fbb0a8ab337efaf
Leandro Carneiro <leandro@carnei.ro>
2019-04-29 20:12:15 -0300

    fix run it

--- run_it.sh
+++ run_it.sh
@@ -1,9 +1,9 @@
-IMAGE=leandrocarneiro/oauth_proxy:zoho
+IMAGE=leandrocarneiro/oauth_proxy:google
 docker rm -vf oauth_proxy ;
 docker run -it --name=oauth_proxy --rm -p 80:80 \
-	-e CLIENT_ID='1000.ABCDEFGHIJKLMNOPQRSTUVWXYZ1234' \
-	-e CLIENT_SECRET='0123456789abcdef0123456789abcdef0123456789' \
-	-e DOMAIN='carnei.ro' \
+	-e CLIENT_ID='123456789012-0a12b3c4asdfasdfasdfasdfasdf6547.apps.googleusercontent.com' \
+	-e CLIENT_SECRET='AYM-abcdEFGHIJkl-adsf_' \
+	-e DOMAIN='gmail.com' \
 	-e TOKEN_SECRET='someRandomHash' \
 	-e UPSTREAM_SITE='http://192.168.2.176:8080' \
 	${IMAGE};
